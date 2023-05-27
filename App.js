3855d341039128c154bfa8023dc2d1decd5f1c4f
NaveenPrajapati <solidauto202@gmail.com>
2023-05-27 15:09:43 +0530

    add stop function

--- App.js
+++ App.js
@@ -1,120 +1,102 @@
-import { StatusBar } from 'expo-status-bar';
-import { useEffect, useRef, useState } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { useState, useEffect,useRef } from 'react';
+import { StyleSheet, Text, Button, View, Pressable, TouchableOpacity } from 'react-native';
 
-export default function App() {
-  const [time, setTime] = useState("00:00:00");
+const Stopwatch = () => {
+  const [time, setTime] = useState('00:00:00');
   const [isRunning, setIsRunning] = useState(false);
-  const startTimeRef = useRef(null);
-  const intervalRef = useRef(null);
+
+  const initialTime=useRef(null)
+  const lapsTime=useRef(null)
 
   useEffect(() => {
-    
-    if (isRunning) {
-      startTimeRef.current = Date.now();
-      intervalRef.current = setInterval(updateTime, 10);
-    } else {
-      clearInterval(intervalRef.current);
+    if(lapsTime.current==0)
+    initialTime.current=Date.now()
+    else{
+      initialTime.current=(Date.now()-lapsTime.current)+initialTime.current
+      lapsTime.current=0
     }
 
-    return () => {
-      clearInterval(intervalRef.current);
-    };
+    if (isRunning) {
+
+
+      const interval = setInterval(() => {
+        const currentTime =Date.now();
+        const elapsedTime=(currentTime-initialTime.current)
+        const seconds = Math.floor((elapsedTime % 60000)/1000);
+        const minutes = Math.floor(elapsedTime / (1000*60));
+        const milliseconds =Math.floor((elapsedTime%1000)/10) ;
+
+        setTime(`${minutes<=9?'0'+minutes:minutes}:${seconds<=9?'0'+seconds:seconds}:${milliseconds.toString().padStart(2, '0')}`);
+      }, 10);
+
+      return () => {
+        clearInterval(interval);
+      };
+    }
   }, [isRunning]);
 
-  const updateTime = () => {
-    const currentTime = Date.now();
-    const elapsedTime = currentTime - startTimeRef.current;
-    const formattedTime = formatTime(elapsedTime);
-    setTime(formattedTime);
-  };
-
-  const handleStart = () => {
-    console.log('start watch',Date.now()/1000)
+  const start = () => {
     setIsRunning(true);
   };
 
-  const handleStop = () => {
-    console.log('stop watch')
+  const stop = () => {
+    
+   lapsTime.current=Date.now()
+
+   console.log(lapsTime.current)
     setIsRunning(false);
-    clearInterval(intervalRef.current)
   };
-
-  const handleReset = () => {
-    console.log('reset watch now')
-    setTime("00:00:00");
+const reset = () => {
     setIsRunning(false);
-    clearInterval(intervalRef.current)
-
-  };
-
-  const formatTime = (time) => {
-    const minutes = Math.floor(time / 60000);
-    const seconds = Math.floor((time % 60000) / 1000);
-    const milliseconds = Math.floor((time % 1000) / 10);
-
-    return `${padNumber(minutes)}:${padNumber(seconds)}:${padNumber(milliseconds)}`;
-  };
-
-  const padNumber = (number) => {
-    return number.toString().padStart(2, '0');
-  };
+    setTime('00:00:00')
+    lapsTime.current==0
+  };  
 
   return (
-    <View style={styles.container}>
-      <View style={styles.timer}>
-        <Text style={styles.time}>{time}</Text>
+    <View className='flex justify-center items-center h-screen'>
+      
+      <Text style={styles.time}>{time}</Text>
+
+      <View className='flex-row  gap-2 p-2 '>
+      <TouchableOpacity
+       className='bg-gray-200 px-3 '
+        onPress={start}
+        disabled={isRunning}><Text>start</Text></TouchableOpacity>
+    
+    <View className='bg-gray-200 px-3'>
+
+      <Button
+        title="Stop"
+        color={'red'}
+        onPress={stop}
+        disabled={!isRunning}
+      />
+    </View>
+    <View>
+
+        <Button
+        title="reset"
+        onPress={reset}
+        disabled={!isRunning}
+      />
+    </View>
       </View>
-      <View style={styles.buttons}>
-        <TouchableOpacity style={styles.button} onPress={handleStart}>
-          <Text style={styles.buttonText}>Start</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={handleStop}>
-          <Text style={styles.buttonText}>Stop</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={handleReset}>
-          <Text style={styles.buttonText}>Reset</Text>
-        </TouchableOpacity>
+      <View>
+
       </View>
-      <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
-  },
-  timer: {
-    width: 200,
-    height: 100,
-    borderRadius: 10,
-    backgroundColor: 'gray',
+    alignItems: 'center',
   },
   time: {
-    fontSize: 36,
-    textAlign: 'center',
-    color: '#ffffff',
+    fontSize: 40,
   },
-  buttons: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    marginTop: 20,
-    gap:2
-  },
-  button: {
-    width: 100,
-    height: 40,
-    borderRadius: 10,
-    backgroundColor: "black",
-    
+});
 
-  },
-  buttonText: {
-    fontSize: 20,
-    textAlign: "center",
-    color: "white",
-  },
-})
+export default Stopwatch;
